8c41455f3a346f969e759fa5ec4c6ae13d21023e
Meg Wilde <meg.c.wilde@gmail.com>
2024-01-05 15:25:15 +0000

    updating files, adding workout details modal

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -22,6 +22,10 @@ const workoutSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  imageURL: {
+    type: String,
+    required: true,
+  },
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
